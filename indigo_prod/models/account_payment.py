a3ee9e59c45bdda9a7e952fa6c5a1e94cd52f64f
Lawrence <jovenlawrencegersaniba.fcu@gmail.com>
2020-05-14 22:06:45 +0800

    name set  readonly = False

--- indigo_prod/models/account_payment.py
+++ indigo_prod/models/account_payment.py
@@ -1,6 +1,10 @@
-from odoo import models, fields, api, _ 
+from odoo import models, fields, api, _
+
 
 class AccountPayment(models.Model):
-	_inherit = 'account.payment'
+    _inherit = 'account.payment'
 
-	cheque_note = fields.Text(string='Cheque Notes')
+    cheque_note = fields.Text(string='Cheque Notes')
+
+    # The name is attributed upon post()
+    name = fields.Char(readonly=False, copy=False)

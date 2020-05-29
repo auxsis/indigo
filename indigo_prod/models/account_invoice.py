a87d47bc8f39846b2649b2a514d0c4f68dd57788
geekrei <reynaflorsentillas@ymail.com>
2020-05-29 00:32:52 +0000

    Updated computation for price unit if a vendor bill is generated from a PO

--- indigo_prod/models/account_invoice.py
+++ indigo_prod/models/account_invoice.py
@@ -16,7 +16,6 @@ class AccountInvoice(models.Model):
     
     exchange_rate = fields.Float(string='Exchange Rate')
     convert_currency = fields.Boolean(compute='_is_convert_currency')
-        #date_due = fields.Date(string='Date Due')
     due_date = fields.Char(string="Date Due", strore=True)
 
     @api.onchange('date_due')
@@ -25,7 +24,6 @@ class AccountInvoice(models.Model):
             if record.date_due:
                 record.due_date = fields.Date.from_string(
                     record.date_due).strftime('%m/%d/%Y')
-
             else:
                 record.due_date = '0'  # datetime.now().strftime('%m/%d/%Y')
 
@@ -71,12 +69,17 @@ class AccountInvoiceLine(models.Model):
         currency = self.invoice_id.currency_id
         type = self.invoice_id.type
         exchange_rate = self.invoice_id.exchange_rate
-        price_unit = self.product_id.lst_price
-        orig_price_unit = self.product_id.lst_price
+        
+        price_unit = self.product_id.standard_price
+        orig_price_unit = self.product_id.standard_price
+        if self.purchase_line_id:
+            orig_price_unit = self.purchase_line_id.price_unit
+            price_unit = self.purchase_line_id.price_unit
+            
         if self.product_id and type in ('in_invoice', 'in_refund'):
             if company and currency:
                 if company.currency_id != currency and exchange_rate:
-                    price_unit = self.product_id.lst_price * exchange_rate
+                    price_unit = price_unit * exchange_rate
         self.price_unit = price_unit
         self.orig_price_unit = orig_price_unit
         result = super(AccountInvoiceLine, self)._compute_price()
@@ -91,13 +94,17 @@ class AccountInvoiceLine(models.Model):
         type = self.invoice_id.type
         exchange_rate = self.invoice_id.exchange_rate
         
-        orig_price_unit = self.product_id.lst_price
-        price_unit = self.product_id.lst_price
-     
+        orig_price_unit = self.product_id.standard_price
+        price_unit = self.product_id.standard_price
+        
+        if self.purchase_line_id:
+            orig_price_unit = self.purchase_line_id.price_unit
+            price_unit = self.purchase_line_id.price_unit
+         
         if self.product_id and type in ('in_invoice', 'in_refund'):
             if company and currency:
                 if company.currency_id != currency and exchange_rate:
-                    price_unit = self.product_id.lst_price * exchange_rate
+                    price_unit = price_unit * exchange_rate
                     
         self.price_unit = price_unit
         self.orig_price_unit = orig_price_unit
